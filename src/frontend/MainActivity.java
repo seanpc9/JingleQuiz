3d28b5bae2903cef7e939d279d1e0716914a9cdd
Divyansh Gupta <divyg@vt.edu>
2014-11-11 18:13:25 -0500

    Added Sofia Support Bruh

--- src/frontend/MainActivity.java
+++ src/frontend/MainActivity.java
@@ -6,10 +6,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity
-    extends Activity
+public class MainActivity extends Activity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
