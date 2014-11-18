d2d1610f894f85cf7d8dd1aa42d49b8610f1126e
seanpc9 <seanpc9@vt.edu>
2014-11-17 21:38:28 -0500

    Welcome Screen Update

--- src/com/example/jinglequiz/WelcomeScreen.java
+++ src/com/example/jinglequiz/WelcomeScreen.java
@@ -15,21 +15,51 @@ import sofia.app.ShapeScreen;
  */
 
 public class WelcomeScreen extends ShapeScreen {
-    	// Fields
-    	private ShapeView shapeView;
-    	private Button easy;
-    	private Button medium;
-    	private Button hard;
-    	private Button start;
+    // Fields
+    private Button easy;
+    private Button medium;
+    private Button hard;
+    private Button start;
+
+    // ----------------------------------------------------------
+    /**
+     * Creates the WelcomeScreen on the screen of the device.
+     */
+    @Override
+    public void initialize() {
+	// Leave empty for now.
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Creates the sets the parameters for an easy game.
+     */
+    public void easyClicked() {
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Creates the sets the parameters for a medium game.
+     */
+    public void mediumClicked() {
+
+    }
+
+    // ----------------------------------------------------------
+    /**
+     * Creates the sets the parameters for a hard game.
+     */
+    public void hardClicked() {
+
+    }
 
 
-	// ----------------------------------------------------------
-	/**
-	 * Creates the WelcomeScreen on the screen of the device.
-	 */
-	@Override
-	public void initialize() {
+    // ----------------------------------------------------------
+    /**
+     * Redirects to the game screen.
+     */
+    public void startClicked() {
 
-	}
-
+    }
 }
